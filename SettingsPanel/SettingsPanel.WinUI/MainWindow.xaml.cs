b0f654f6c5f78f658f0377c54308b60f20dbf95e
U+5BFA <onein528@outlook.com>
2023-02-09 01:17:58 +0900

    Add project files.

---
+++ SettingsPanel/SettingsPanel.WinUI/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using WinRT.Interop;
+using WinUIEx;
+
+namespace SettingsPanel
+{
+	public sealed partial class MainWindow : WindowEx
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			PersistenceId = "MainWindow";
+
+			EnsureEarlyWindow();
+		}
+
+		private void EnsureEarlyWindow()
+		{
+			// Set title
+			AppWindow.Title = "SettingsPanel";
+
+			// Set icon
+			AppWindow.SetIcon(Path.Combine(Windows.ApplicationModel.Package.Current.InstalledLocation.Path, "Assets/AppTiles/Release/StoreLogo.scale-400.png"));
+
+			// Extend title bar
+			AppWindow.TitleBar.ExtendsContentIntoTitleBar = true;
+
+			// Set window buttons background to transparent
+			AppWindow.TitleBar.ButtonBackgroundColor = Colors.Transparent;
+			AppWindow.TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+			// Set min size
+			base.MinHeight = 328;
+			base.MinWidth = 516;
+		}
+	}
+}
